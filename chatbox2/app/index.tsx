10d8fb2f96927e971e4988a04c16fac6ee275ec5
ab-h-i-n <abhinshelly@gmail.com>
2024-09-29 22:51:20 +0530

    almost

--- chatbox2/app/index.tsx
+++ chatbox2/app/index.tsx
@@ -1,9 +1,80 @@
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from "react";
+import NetInfo from '@react-native-community/netinfo';
+import { socket } from "@/utils/Socket";
 
 export default function Index() {
+
+  const [isConnected, setConnected] = useState(false);
+  const [isSocketConnected, setSocketConnected] = useState(false);
+  const [type, setType] = useState<string | null>(null); 
+
+  const storeData = async (value: any) => {
+    try {
+      await AsyncStorage.setItem('my-key', value);
+    } catch (e) {
+      // saving error
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    storeData('hello world');
+    const unsubscribe = NetInfo.addEventListener(state => {
+      setType(state.type);
+      setConnected(state.isInternetReachable || false);
+      console.log('Connection type', state.type);
+      console.log('Is connected?', state.isConnected);
+    });
+
+    return () => {
+      unsubscribe(); 
+    };
+  }, []);
+
+
+  useEffect(() => {
+    if (socket.connected) {
+      onConnect();
+    }
+
+    function onConnect() {
+      setSocketConnected(true);
+    }
+
+    function onDisconnect() {
+      setSocketConnected(false);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+    };
+  }, []);
+
   return (
-    <View className="flex-1 items-center justify-center bg-red-500">
-      <Text className="text-4xl font-semibold text-white">Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView className="h-full">
+      <View className="flex-1 items-center justify-center bg-red-500">
+        <Text className="text-4xl font-semibold text-white">Edit app/index.tsx to edit this screen.</Text>
+        {
+          isSocketConnected ? (
+            <Text className="text-4xl font-semibold text-green-500">Connected to socket</Text>
+          ) : (
+            <Text className="text-4xl font-semibold text-yellow-500">Not connected to socket</Text>
+          )
+        }
+        <Text className="text-2xl font-medium text-white mt-4">
+          Connection Type: {type ?? 'Unknown'}
+        </Text>
+        <Text className="text-2xl font-medium text-white mt-2">
+          Status: {isConnected ? 'Online' : 'Offline'}
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
---
+++ chatbox2/utils/Socket.ts
@@ -0,0 +1,3 @@
+import { io } from 'socket.io-client';
+
+export const socket = io('https://chatbox-app-0nx2.onrender.com'); 
