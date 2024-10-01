438ff97f3d46dd2bba28c2b3780edf2808c5fae7
ab-h-i-n <abhinshelly03@gmail.com>
2024-10-01 17:49:14 +0530

    fix : icons module not found issue ts

--- chatbox2/@types/declarations.d.ts
+++ chatbox2/@types/declarations.d.ts
@@ -1,4 +1,5 @@
 import 'react-native';
+import { ImageSourcePropType } from 'react-native';
 
 // Extend props for all relevant React Native components
 declare module 'react-native' {
@@ -16,6 +17,21 @@ declare module 'react-native' {
 
   interface ImageProps {
     className?: string;
+  }
+
+  declare module '*.png' {
+    const value: ImageSourcePropType;
+    export default value;
+  }
+  
+  declare module '*.jpg' {
+    const value: ImageSourcePropType;
+    export default value;
+  }
+  
+  declare module '*.jpeg' {
+    const value: ImageSourcePropType;
+    export default value;
   }
 
   // Add any other components you want to extend here
