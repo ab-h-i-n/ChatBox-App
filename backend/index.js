be665598eef34defb50a7945041c2c422392cb72
ab-h-i-n <abhinshelly03@gmail.com>
2024-10-02 13:20:18 +0530

    added a ping function

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,33 @@
 import "dotenv/config";
 import { app, server } from "./routes/socket.js";
 import { router as roomRoutes } from "./routes/roomRoutes.js";
+import log from "./utils/log.js";
 
 app.use("/rooms", roomRoutes);
 
 server.listen(process.env.PORT || 4000, () => {
   console.log(`Server is running on ${process.env.PORT || 4000}`);
+  setInterval(() => {
+    Ping();
+    ping++;
+  }, 30000);
 });
 
 app.get("/", (req, res) => {
   res.send("Server is running");
+
 });
+
+var ping = 0;
+app.get("/ping", (req, res) => {
+  log({
+    ping: ping,
+  });
+  res.json({
+    ping: ping,
+  });
+});
+
+async function Ping() {
+  await fetch(`http://localhost:4000/ping`);
+}
